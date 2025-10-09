02479632019ddacb244e7b11ad283e0df2da2535
Jeff Smith <ToxicGLaDOS@gmail.com>
2025-10-09 16:45:55 -0500

    Remove deprecated lsp-zero
    [They're dead!](https://github.com/VonHeikemen/lsp-zero.nvim) because
    nvim supports this natively now, so let's use the native features. I
    haven't tested every one of these plugins, but `nvim` no longer errors
    when I open it, so the rest can wait until I need them.

--- papaya/nvim/init.lua
+++ papaya/nvim/init.lua
@@ -50,8 +50,6 @@ vim.g.maplocalleader = "\\"
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    -- add your plugins here
-    {'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'},
     {'neovim/nvim-lspconfig'},
     {'hrsh7th/cmp-nvim-lsp'},
     {'hrsh7th/nvim-cmp'},
@@ -63,37 +61,26 @@ require("lazy").setup({
   checker = { enabled = true },
 })
 
-local lsp_zero = require('lsp-zero')
-
 local lsp_attach = function(client, bufnr)
   -- this is where you enable features that only work
   -- if there is a language server active in the file
-  lsp_zero.default_keymaps({buffer = bufnr})
   -- Open error float with C-e
   vim.keymap.set('n', '<C-e>', function() vim.diagnostic.open_float() end, {noremap = true})
 end
 
-lsp_zero.extend_lspconfig({
-  sign_text = true,
-  lsp_attach = lsp_attach,
-})
-
-require('lspconfig').volar.setup({})
-local lspconfig = require('lspconfig')
-
-lspconfig.volar.setup {
+vim.lsp.enable('vue_ls', {
   filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
   init_options = {
     vue = {
       hybridMode = false,
     },
   },
-}
+})
 
-require('lspconfig').gopls.setup({})
-require('lspconfig').rust_analyzer.setup({})
-require('lspconfig').lua_ls.setup({})
-require('lspconfig').gdscript.setup({})
+vim.lsp.enable('gopls')
+vim.lsp.enable('rust_analyzer')
+vim.lsp.enable('lua_ls')
+vim.lsp.enable('gdscript')
 
 
 local cmp = require('cmp')
